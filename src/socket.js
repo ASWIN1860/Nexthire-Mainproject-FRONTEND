b9ac498bd5b2fe9962bc2d622bd7ce40bad2691a
Aswin Suresh <aswin3577@gmail.com>
2026-05-23 21:27:16 +0530

    render link updated in socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
+import base_url from "./services/base_url";
 
-const socket = io("http://localhost:3000");
+const socket = io(base_url);
 
 export default socket;
